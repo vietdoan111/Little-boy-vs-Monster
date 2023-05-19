1c16279d636ea127d0d39bb7323ace50bf847d86
phamViet <doantotnghiep2023game2d@gmail.com>
2023-05-19 14:54:47 +0700

    add composite flock behavior

--- Assets/Script/Enemy/Flocking/Behavior Script/SteeredCohesion.cs
+++ Assets/Script/Enemy/Flocking/Behavior Script/SteeredCohesion.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Flock/Behavior/Cohesion")]
-public class CohesionBehavior : FlockBehavior
+[CreateAssetMenu(menuName = "Flock/Behavior/Steered Cohesion")]
+public class SteeredCohesion : FlockBehavior
 {
+    Vector2 currentVelocity;
+    public float agentSmoothTime = 0.5f;
+
     public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
     {
         if (context.Count == 0) return Vector2.zero;
@@ -19,6 +22,7 @@ public class CohesionBehavior : FlockBehavior
 
         //create offset from agent position
         cohesionMove -= (Vector2)agent.transform.position;
+        cohesionMove = Vector2.SmoothDamp(agent.transform.up, cohesionMove, ref currentVelocity, agentSmoothTime);
 
         return cohesionMove;
     }
