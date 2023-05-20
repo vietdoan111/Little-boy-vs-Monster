5f3182f48a47890e4ce667fa9f3ff6af2c5af0ea
phamViet <doantotnghiep2023game2d@gmail.com>
2023-05-20 18:29:44 +0700

    turn off custom editor, add simple targeting behavior

--- Assets/Script/Enemy/Flocking/Editor/CompositeBehaviorEditor.cs
+++ Assets/Script/Enemy/Flocking/Editor/CompositeBehaviorEditor.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(CompositeBehavior))]
+//[CustomEditor(typeof(CompositeBehavior))]
 public class CompositeBehaviorEditor : Editor
 {
     public override void OnInspectorGUI()
--- Assets/Script/Enemy/Flocking/FlockAgent.cs
+++ Assets/Script/Enemy/Flocking/FlockAgent.cs
@@ -6,17 +6,20 @@ using UnityEngine;
 public class FlockAgent : MonoBehaviour
 {
     Collider2D agentCollider;
+    Transform target;
     public Collider2D AgentCollider { get { return agentCollider; } }
 
     // Start is called before the first frame update
     void Start()
     {
         agentCollider = GetComponent<Collider2D>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     public void Move(Vector2 velocity)
     {
         transform.up = velocity;
-        transform.position += (Vector3) velocity * Time.deltaTime;
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(((Vector3)velocity + direction.normalized) * Time.deltaTime);
     }
 }
