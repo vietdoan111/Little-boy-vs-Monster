adeaed53bca65ff6ce1b2f09bf076fb4a8dd6367
phamViet <doantotnghiep2023game2d@gmail.com>
2023-06-04 16:28:53 +0700

    add setting, exit, back button function

--- Assets/Script/UI/Menu.cs
+++ Assets/Script/UI/Menu.cs
@@ -5,8 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public VectorValue startPos;
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Map1");
+        startPos.defaultValue.x = 17.0f;
+        startPos.defaultValue.y = -12.0f;
+        startPos.initialValue = startPos.defaultValue;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
     }
 }
