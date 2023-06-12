663702db8fe0deb1bbe105b1baa88dc2e2aa80a8
phamViet <doantotnghiep2023game2d@gmail.com>
2023-06-12 10:19:51 +0700

    fix

--- Assets/Script/Enemy Weapon/EnemyProjectile.cs
+++ Assets/Script/Enemy Weapon/EnemyProjectile.cs
@@ -7,12 +7,17 @@ public class EnemyProjectile : MonoBehaviour
     public float maxFlyingTime;
     public float maxAppearingTime;
     public Vector2 targetPos;
+    public float maxNonTriggerTime;
+    public Collider2D col;
 
     float waitTime = 0.0f;
 
     private void FixedUpdate()
     {
         waitTime += Time.fixedDeltaTime;
+
+        if (waitTime > maxNonTriggerTime) col.isTrigger = false;
+
         if (waitTime < maxFlyingTime)
             transform.position = Vector2.MoveTowards(transform.position, targetPos, 7.0f * Time.fixedDeltaTime);
 
